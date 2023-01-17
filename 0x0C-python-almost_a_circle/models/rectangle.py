fadd496b15bc4be66439d6da9c4226e4db26168a
Anthonytrisha <anthonytrisha04@gmail.com>
2023-01-17 01:26:23 -0800

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,104 +1,134 @@
 #!/usr/bin/python3
 """
-Class Module
+    rectangle.py module
 """
 from models.base import Base
+import json
 
 
 class Rectangle(Base):
-    """Rectangle subclass"""
+    """Rectangle class"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """instance initialization method
-
-        args:
-            width: width of rectangle
-            height: height of rectangle
-            x: init variable
-            y: init variable
-        """
-        super().__init__(id)
+        """Iniciatation method"""
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-
-    def area(self):
-        """return area of width & height"""
-        return self.width * self.height
-
-    def update(self, *args, **kwargs):
-        """update attributes"""
-        if args:
-            listme = ['id', 'width', 'height', 'x', 'y']
-            i = 0
-            for arg in args:
-                setattr(self, listme[i], arg)
-                i += 1
-        else:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-
-    def display(self):
-        """print into stdout
-        return: na
-        """
-        for row in range(self.y):
-            print()
-        for row in range(self.height):
-            print("{}{}".format(" " * self.x, "#" * self.width))
-
-    def __str__(self):
-        """print method"""
-        return "[{}] ({}) {}/{} - {}/{}".format(self.__class__.__name__,
-                                                self.id, self.__x, self.__y,
-                                                self.__width, self.__height)
+        super().__init__(id)
 
     def to_dictionary(self):
-        """return dict representation of Rectangle"""
-        return {'x': self.x, 'y': self.y, 'id': self.id,
-                'height': self.height, 'width': self.width}
+        """Returns the dictionary representation of a Rectangle"""
+        idd = self.id
+        x = self.__x
+        y = self.__y
+        w = self.__width
+        h = self.__height
+        return {'x': x, 'y': y, 'id': idd, 'height': h, 'width': w}
+
+    def update(self, *args, **kwargs):
+        """Assigns an argument to each attribute of the class"""
+        if len(args) > 0:
+            for num, arg in enumerate(args):
+                if num == 0:
+                    self.id = arg
+                elif num == 1:
+                    self.width = arg
+                elif num == 2:
+                    self.height = arg
+                elif num == 3:
+                    self.x = arg
+                elif num == 4:
+                    self.y = arg
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "width" in kwargs:
+                self.width = kwargs["width"]
+            if "height" in kwargs:
+                self.height = kwargs["height"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
+
+    def __str__(self):
+        """returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
+        idd = self.id
+        x = self.__x
+        y = self.__y
+        w = self.__width
+        h = self.__height
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(idd, x, y, w, h)
+
+    def display(self):
+        """ Return the rectangle with the character #"""
+        s = ""
+        if self.__height != 0 and self.__width != 0:
+            s += '\n' * self.__y
+            for i in range(self.__height):
+                s += ' ' * self.__x
+                s += ("#" * self.__width)
+                if i != self.__height - 1:
+                    s += '\n'
+        print(s)
+
+    def area(self):
+        """Return the rectangle's Area"""
+        return self.__width * self.__height
 
     @property
     def width(self):
-        """width getter method"""
+        """Width getter"""
         return self.__width
 
     @width.setter
-    def width(self, value):
-        """width setter method"""
-        self.integer_validator('width', value)
-        self.__width = value
+    def width(self, width):
+        """Width setter"""
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = width
 
     @property
     def height(self):
-        """height getter method"""
+        """Height getter"""
         return self.__height
 
     @height.setter
-    def height(self, value):
-        """height setter method"""
-        self.integer_validator('height', value)
-        self.__height = value
+    def height(self, height):
+        """Height setter"""
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = height
 
     @property
     def x(self):
-        """x getter method"""
+        """x setter"""
         return self.__x
 
     @x.setter
-    def x(self, value):
-        """x setter method"""
-        self.integer_validator2('x', value)
-        self.__x = value
+    def x(self, x):
+        """x setter"""
+        if type(x) != int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = x
 
     @property
     def y(self):
-        """y getter method"""
+        """y getter"""
         return self.__y
 
     @y.setter
-    def y(self, value):
-        """y setter method"""
-        self.integer_validator2('y', value)
-        self.__y = value
+    def y(self, y):
+        """y setter"""
+        if type(y) != int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = y
